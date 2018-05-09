ff51f0282d0942004354e9d2a67c91ee2781255a
Rafael Novello <rafa.reis.novello@gmail.com>
2018-05-09 17:01:58 -0300

    AutoML engine working

--- automl-example-engine/marvin_automl_example_engine/prediction/predictor.py
+++ automl-example-engine/marvin_automl_example_engine/prediction/predictor.py
@@ -23,6 +23,9 @@ class Predictor(EngineBasePrediction):
         super(Predictor, self).__init__(**kwargs)
 
     def execute(self, input_message, params, **kwargs):
-        final_prediction = self.marvin_model["clf"].predict(input_message)[0]
+        import numpy as np
+        input_message = np.array(input_message)
+        pred = self.self.marvin_model["clf"].predict(input_message)[0]
+        final_prediction = self.self.marvin_model["encoder"].classes_[pred]
 
         return final_prediction
